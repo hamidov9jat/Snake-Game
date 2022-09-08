4aef4add9d9702faabf29cefc9f78fade7f54985
Nijat Hamidov <hamidov9jat@gmail.com>
2022-09-08 12:26:08 +0400

    already implemented snake

---
+++ main.py
@@ -0,0 +1,32 @@
+# import turtle
+import time
+from turtle import Screen
+from snake import Snake
+
+screen = Screen()
+screen.setup(600, 600)
+# screen.screensize()
+print(screen.canvwidth, screen.canvheight)
+# screen.bgcolor('black')
+screen.title('Snake Game')
+screen.tracer(5)
+
+sn = Snake(screen)
+
+screen.listen()
+screen.onkey(key='Up', fun=sn.up)
+screen.onkey(key='Down', fun=sn.down)
+screen.onkey(key='Left', fun=sn.left)
+screen.onkey(key='Right', fun=sn.right)
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    sn.move()
+
+screen.exitonclick()
+
+
+# screen.bye()
+# turtle.done()
---
+++ snake.py
@@ -0,0 +1,89 @@
+from turtle import Turtle
+from turtle import Screen
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self, sn_screen: Screen):
+        self.segments = []
+        self.create_snake()
+        self.head: Turtle = self.segments[0]
+        self.screen = sn_screen
+        self.scn_w = sn_screen.window_width()
+        self.scn_h = sn_screen.window_height()
+
+    def create_snake(self):
+        """Create a snake of length 3 (squares)"""
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle('square')
+            new_segment.penup()
+            new_segment.goto(position)
+            # new_segment.speed(0)
+            # print(new_segment.get_shapepoly())
+            # new_segment.stamp()
+            # print(f"1.{new_segment.pos()=}")
+            # print(new_segment.shearfactor())
+            # new_segment.color('green')
+            # # new_segment.shearfactor(-1.5)
+            # print(f"{new_segment.pos()=}")
+            # print(new_segment.get_shapepoly())
+            # increase = tuple(2 * num for num in new_segment.turtlesize())
+            # print(new_segment.turtlesize(*increase))
+            # new_segment.color('black')
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            is_out_horizontally = abs(new_x) > (self.scn_w / 2)
+            is_out_vertically = abs(new_y) > (self.scn_h / 2)
+
+            opposite_x = -(self.scn_w / 2) + new_x % (self.scn_w / 2)
+            opposite_y = -(self.scn_h / 2) + new_y % (self.scn_h / 2)
+
+            if is_out_horizontally:
+                self.segments[seg_num].setx(opposite_x if new_x >= 0 else new_x % (self.scn_w / 2))
+            if is_out_vertically:
+                self.segments[seg_num].sety(opposite_y if new_y >= 0 else new_y % (self.scn_h / 2))
+            if not is_out_horizontally and not is_out_vertically:
+                self.segments[seg_num].goto(new_x, new_y)
+
+        new_head_x = self.head.xcor() + MOVE_DISTANCE
+        new_head_y = self.head.ycor() + MOVE_DISTANCE
+
+        is_out_horizontally = abs(new_head_x) > (self.scn_w / 2)
+        is_out_vertically = abs(new_head_y) > (self.scn_h / 2)
+
+        opposite_x = -(self.scn_w / 2) + new_head_x % (self.scn_w / 2)
+        opposite_y = -(self.scn_h / 2) + new_head_y % (self.scn_h / 2)
+
+        if is_out_horizontally:
+            self.head.setx(opposite_x if new_head_x >= 0 else new_head_x % (self.scn_w / 2))
+        if is_out_vertically:
+            self.head.sety(opposite_y if new_head_y >= 0 else new_head_y % (self.scn_h / 2))
+        if not is_out_horizontally and not is_out_vertically:
+            self.head.forward(MOVE_DISTANCE)
+        # self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
