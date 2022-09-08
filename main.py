cb9987bd9f1636b192f0598bddc8fb2890b6a507
Nijat Hamidov <hamidov9jat@gmail.com>
2022-09-08 13:46:57 +0400

    game is over when the snake bites itself

--- main.py
+++ main.py
@@ -27,14 +27,26 @@ while game_is_on:
     time.sleep(0.1)
     sn.move()
 
+    # Detect collision with tail.
+    for segment in sn.segments:
+        if segment == sn.head:
+            continue
+        elif sn.head.distance(segment) < 10:
+            game_is_on = False
+            # scoreboard.game_over()
+
+        # Detect collision with wall.
+        # if sn.head.xcor() > 280 or sn.head.xcor() < -280 or sn.head.ycor() > 280 or sn.head.ycor() < -280:
+        #     game_is_on = False
+        #     # scoreboard.game_over()
+
     # Detect collision with food.
     if sn.head.distance(fd) < 15:
         fd.refresh()
-        # sn.extend()
+        sn.extend()
         # scoreboard.increase_score()
 
 screen.exitonclick()
-
 
 # screen.bye()
 # turtle.done()
